4cdd838ff9e023b9d4196b1db401466476c1c4dc
Nick <ndugal1@lsu.edu>
2017-12-06 21:11:32 -0600

    Fixed Week 1 issue

--- real.py
+++ real.py
@@ -3,14 +3,23 @@ import os, errno
 import numpy as np
 
 
-#WEEK 1 IS BROKEN CANT RABBIT HOLE INTO FIXING, WILL FIGURE OUT LATER
 """"
 FINAL VERSION OF AI PROJECT FOR SUBMISSION. REALER THAN REAL MY DUDES. NICHOLAS DUGAL
 """
-def main(dataPath):
 
-    position = 'qb'             # Hardcoded position will change
-    posPath = dataPath + '/'+position     # Path to position data
+def main():
+    for pos in ['rb','wr','te','dst']:
+        createAveragedData(pos=pos)
+
+def makePositionsAveraged():
+    for pos in ['rb','wr','te','dst','qb']:
+        createAveragedData(pos=pos)
+
+# Creates dfs saved as csv for each player that has their average for each week for each year,
+# Each level is saved meaning directory structure is: data -> position (csv for all players) -> specificPlayer -> (csv for all years) -> specificYear (csv for all weeks)
+def createAveragedData(pos='qb',dataPath='/Users/nickdugal/documents/fantasy-football/data'):
+
+    posPath = dataPath + '/'+pos     # Path to position data
     ensure_dir(posPath)            # Create path if none exists
     os.chdir(posPath)
     updatedQBlist = []                  # This will hold updated df per player to concate and make updated qb DF
@@ -37,7 +46,7 @@ def main(dataPath):
     posDF = pd.concat(updatedQBlist)
 
     posDF.to_csv(posPath+'/AveragedData.csv')
-    df.to_csv(posPath+'ActualData.csv')
+    df.to_csv(posPath+'/ActualData.csv')
 
    #Uncomment below after testing
         # updatedYearList = [makeAverage(year,subData) for year, subData in data.groupby('Year')]
@@ -63,6 +72,8 @@ def makeAverage(year, yearData):
     for alpha in alphaColumns: #So we run the same as above, except don't do math, just copy data over
         newDF[alpha] = yearData[alpha]
     newDF['Week'] = yearData['Week']
+    firstWeek = (newDF['Week'].tolist())[0]
+    newDF[newDF.Week == firstWeek] = yearData[yearData.Week == firstWeek]
 
     return newDF
 
@@ -89,4 +100,4 @@ def removeAlphaData(unCleanedDF):
     return unCleanedDF[list(purgeAlphas(unCleanedDF))]
 
 
-if __name__ == '__main__': main(dataPath='/Users/nickdugal/documents/fantasy-football/data')
+if __name__ == '__main__': main()
